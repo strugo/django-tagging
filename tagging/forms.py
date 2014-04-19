7c8a6832d45a271f03a42119b1908023626995ba
Gary Wilson Jr <gary@thegarywilson.com>
2014-04-19 01:07:30 -0500

    Fixed several deprecation warnings.

--- tagging/forms.py
+++ tagging/forms.py
@@ -13,6 +13,7 @@ from tagging.utils import parse_tag_input
 class TagAdminForm(forms.ModelForm):
     class Meta:
         model = Tag
+        fields = ['name']
 
     def clean_name(self):
         value = self.cleaned_data['name']
